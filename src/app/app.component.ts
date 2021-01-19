aef6396d16ddb7d9bec821fcbf31c80e3afab4d9
Johannes <johannessimmet@gmail.com>
2021-01-19 07:40:58 +0100

    gesamt hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  auto_co2 = 0;
+  km
+  verbrauch
+  co2prokm = 0;
+  auto() {
+    this.auto_co2 = this.km * this.co2prokm * this.verbrauch
+    this.co2auto.push(this.auto_co2)
+  }
+
+
+  co2auto = [0,];
+
   title = 'co2';
 
   aut = ["1"];
   autzahl = 1;
 
-  co2prokm = 0;
-  km = 0;
-  verbrauch = 0;
-  auto_co2 = 0;
+  co2prokmu;
+  kmu;
+  verbrauchu;
+  auto_co2u = 0;
+
+  autou() {
+    this.auto_co2u = this.kmu * this.co2prokmu * this.verbrauchu
+
+  }
+
   kwh
   co2prokwh = 0;
   leit_h2o_ver
@@ -36,55 +55,101 @@ export class AppComponent {
   ernährungsplatzhalter
 
   heizart
-  co2prom3holz = 1;//wert los
-  co2prolöl = 2;//wert los
-  Co2prom3gas = 3;//wert los
+
+
   co2heizung
 
   Bahn_km;
   heizungtest = 0;
   Rbus_km;
-  Reisebusco2= 20;
-  co2prokmBahn= 40;
+  Reisebusco2 = 20;
+  co2prokmBahn = 40;
   co2prolleitung = 0.3;
   co2prolflasche = 211;
   co2prokmflug = 369;
   g_pro_gbit = 7076;
-  holzco2 = 1;      //hier echten wert einsetzen
+  holzco2 = 1830000;
 
   mengeneinheitheizung = "m³"
 
-  authinzu(){
+  authinzu() {
     this.aut.push("1");
-  this.autzahl =this.autzahl +1;
+    this.autzahl = this.autzahl + 1;
   }
 
- strom() {
-    this.strom_co2 = this.kwh * this.co2prokwh
+  strom() {
+    this.strom_co2 = this.kwh * this.co2prokwh/this.Personen_im_Haushalt
     console.log(this.strom_co2)
- } 
+  }
+  urlaub_co2:number  = 0;
+  urlaub() {
+    this.urlaub_co2 = this.flug_km * this.co2prokmflug + this.Bahn_km * this.co2prokmBahn + this.Rbus_km * this.Reisebusco2 + this.kmu * this.verbrauchu * this.co2prokmu
+  }
 
   wasser() {
-    this.leit_h2o_co2 = this.leit_h2o_ver * this.co2prolleitung
+    this.leit_h2o_co2 = this.leit_h2o_ver * this.co2prolleitung/this.Personen_im_Haushalt
     this.fla_h2o_co2 = this.fla_h2o_ver * this.co2prolflasche
     this.co2_h2o = this.leit_h2o_co2 + this.fla_h2o_co2
     console.log(this.co2_h2o)
   }
 
-
-  heizung() {
-
-    this.heizungtest = this.heitzmenge * this.heizart
-
-    if (this.heizart === "co2prolöl") {
-      console.log(this.co2prolöl);
-      this.mengeneinheitheizung = "l"
-    } else if (this.heizart === "co2prokwh") {
-      console.log(this.co2prokwh);
-      this.mengeneinheitheizung = "kwh"
-    } else if (this.heizart === "40") {
-      console.log(this.co2prokwh);
-      this.mengeneinheitheizung = ""
-    } else this.mengeneinheitheizung = "m³";
+  internet() {
+    this.internet_co2 = this.internet_gbit * this.g_pro_gbit
   }
+  heiz_co2:number = 0;
+  heizung() {
+    this.heizungtest = this.heitzmenge * this.heizart
+    if (this.heizart === "1830000") {
+      this.mengeneinheitheizung = "m³ Festmeter"
+    } else if (this.heizart === "2650") {
+      this.mengeneinheitheizung = "l"
+    } else if (this.heizart === "1630") {
+      this.mengeneinheitheizung = "m³"
+    } else if (this.heizart === "40") {
+      this.mengeneinheitheizung = ""
+    }
+    this.heiz_co2 = this.heitzmenge * this.heizart/this.Personen_im_Haushalt
+  }
+
+  Mahlzeiten_mit_Fleisch;
+  co2promahlmitfl = 2294;
+  Mahlzeiten_ohne_Fleisch;
+  co2promahlohnefl = 76.5;
+  ernarungs_co2:number = 0;
+  Ernahrung() {
+    this.ernarungs_co2 = this.Mahlzeiten_mit_Fleisch * this.co2promahlmitfl + this.Mahlzeiten_ohne_Fleisch * this.co2promahlohnefl
+  }
+  co2_shuhe = 1692;
+  co2_kleidung = 10587.5;
+  Neue_Kleidungsstucke;
+  Neue_Schuhe;
+  kleidung_co2:number = 0;
+  Kleidung() {
+    this.kleidung_co2 = this.Neue_Kleidungsstucke * this.co2_kleidung + this.Neue_Schuhe * this.co2_shuhe
+
+  }
+  co2gesamt1;
+  co2gesamt2;
+  co2gesamt3;
+  co2gesamt;
+
+  co2gesamtkg;
+  co2gesamtt;
+  gesamt() {
+
+    this.co2gesamt1 = this.auto_co2 + this.strom_co2 + this.co2_h2o 
+this.co2gesamt2=this.urlaub_co2 + this.internet_co2 
+this.co2gesamt3=this.heiz_co2 + this.ernarungs_co2 + this.kleidung_co2
+    this.co2gesamt=this.co2gesamt1+    this.co2gesamt2 +    this.co2gesamt3 + 1104000
+    console.log(this.co2gesamt)
+    this.co2gesamtkg= Math.floor(this.co2gesamt/1000);
+    console.log(this.co2gesamtkg)
+    this.co2gesamtt= Math.floor(this.co2gesamtkg/1000);
+    console.log(this.co2gesamtt)
+
+
+  }
+
+
+
 }
